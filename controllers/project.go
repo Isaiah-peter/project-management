903053502c04db78c6dfc48715c2c903b6fbedc6
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-01-09 21:43:55 +0100

    create project

---
+++ controllers/project.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"project-management/models"
+	"project-management/utils"
+	"strconv"
+)
+
+func CreateProject(w http.ResponseWriter, r *http.Request) {
+	token := utils.UseToken(r)
+	newProject := &models.AddProjctModel{}
+	Id, err := strconv.ParseInt(fmt.Sprintf("%f", token["UserId"]), 0, 0)
+	if err != nil {
+		log.Panic(err)
+	}
+	if err := json.NewDecoder(r.Body).Decode(newProject); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("unable to decode json"))
+	}
+	newProject.UserId = Id
+	u := newProject.CreateProject()
+	res, errM := json.Marshal(u)
+	if errM != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("error while sending data back"))
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
